b133bb7977b6e1d65f949c3df4307f13642cb3f0
awchjimmy <awchjimmy@gmail.com>
2020-07-06 17:04:01 +0800

    加入 lifethin

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const vueApp = new Vue({
+  el: "#vue-app",
+  data: function () {
+    return {
+      tableData: [
+        {
+          date: "2020-05-27",
+          codename: "lifethin",
+          description: "壽瘦GI酮 官方網站",
+          fossUrl: "https://github.com/lifethin/lifethin.github.io",
+        },
+      ],
+    };
+  },
+});
